e3793ec0a0a9d91b9ebe89bff384cf1e8c18455a
AaronTan <aarontys1999@gmail.com>
2023-05-21 23:14:58 +0800

    remove /react for import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import { ThemeProvider } from 'next-themes'
-import {Analytics} from '@vercel/analytics/react;'
+import {Analytics} from '@vercel/analytics'
 
 export default function App({ Component, pageProps }) {
   return(
